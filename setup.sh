866a7c15c0fc5d483352c1a53f5b225928760837
A-Xamon <miniterling@gmail.com>
2025-10-29 19:50:26 +0100

    Explanation title of the file

---
+++ setup.sh
@@ -0,0 +1,2 @@
+# This file makes the installation of Packet Tracer better, here I will explain step by step how it installs
+echo "Welcome to the installation of Packet Tracer in a RHEL based system"
