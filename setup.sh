c7cae02bccce975a170ce13139a4e37c4408a4cf
A-Xamon <miniterling@gmail.com>
2025-10-30 17:02:17 +0100

    Auto install file

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 # This file makes the installation of Packet Tracer better, here I will explain step by step how it installs
-
-black='\033[0;30m'
-red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
-blue='\033[0;34m'
 cian='\033[0;36m'
-white='\033[0;37m'
 RESET='\033[0m'
 clear
 #Title
